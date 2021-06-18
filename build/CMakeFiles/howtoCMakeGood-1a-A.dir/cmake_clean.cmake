b145b1b7c8d090716eecf49ace7bb97f6c2ebe7f
cl-rs <tucater10896601@protonmail.com>
2021-06-18 05:49:47 +0530

    FirstCmake Tutorial

---
+++ build/CMakeFiles/howtoCMakeGood-1a-A.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/howtoCMakeGood-1a-A.dir/main.cpp.o"
+  "howtoCMakeGood-1a-A"
+  "howtoCMakeGood-1a-A.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/howtoCMakeGood-1a-A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
